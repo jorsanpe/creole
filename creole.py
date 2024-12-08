f74ddf819f6595c1f38ae01c8a2e7ffefa5c782a
Jordi Sánchez <jorsanpe@gmail.com>
2024-12-08 21:15:34 +0100

    Implemented command line argument invokation

--- creole.py
+++ creole.py
@@ -1,12 +1,26 @@
+#!/usr/bin/env python
+import argparse
 import sys
 from creole import entry
 
 
+def argument_parser():
+    parser = argparse.ArgumentParser(prog='creole', description='invoke the creole parser')
+    parser.add_argument('-o', '--output-file', help='output file')
+    parser.add_argument('-l', '--language', help='output language', default="C", choices=["C", "python"])
+    parser.add_argument('input_file', help='input files')
+    return parser
+
+
 def main(argv):
-    with open(argv[1], "r") as stream:
+    parser = argument_parser()
+    args = parser.parse_args(argv)
+
+    with open(args.input_file, "r") as stream:
         source_code = stream.read()
-    entry.transpile(source_code, "python")
+    transpiled = entry.transpile(source_code, args.language)
+    print(transpiled)
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main(sys.argv[1:])
